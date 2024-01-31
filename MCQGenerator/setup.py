c087495a6d4cddedcda2eab20e2843433603b5c0
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-01-31 05:55:01 +0530

    file updates

---
+++ MCQGenerator/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'mcqgenerator',
+    version='0.0.1',
+    author='bhavesh padharia',
+    author_email='padharia.bhavesh06@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+
+)
